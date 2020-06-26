f53d27955a8e4d26f19e9a62b513c3feea84fa62
Douglas <douglas.lisboa@edu.unipar.br>
2020-06-25 23:10:49 -0300

    Implementado conceito de Solid, Dependency Invertion Principal.

---
+++ src/service/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import Appointment from '../models/Appointment';
+import appointmentRepository from '../repositories/AppointmentsRepository';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import { startOfHour } from 'date-fns';
+
+interface RequestDTO {
+  date: Date;
+  provider: string;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider}:RequestDTO): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+        appointmentDate,
+      );
+
+    if(findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
